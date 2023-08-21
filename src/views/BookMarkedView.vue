966a00331a9a4333df35719b01649223fc735ecf
cristinakellyt <cristinakellyt@gmail.com>
2023-08-21 13:50:56 +0200

    style(views): implement code style in app views

--- src/views/BookMarkedView.vue
+++ src/views/BookMarkedView.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>bookmarked</h1>
+  <h2 class="heading-secondary">Bookmarked</h2>
+  <div class="grid-layout">
+    <MovieCard
+      v-for="movie in bookmarkedList"
+      :key="movie.title"
+      :category="movie.category"
+      :isBookmarked="movie.isBookmarked"
+      :isTrending="movie.isTrending"
+      :thumbnail="movie.thumbnail"
+      :title="movie.title"
+      :rating="movie.rating"
+      :year="movie.year"
+      class="movie-card"
+    />
+  </div>
 </template>
+
+<script setup>
+import MovieCard from '@/components/MovieCard.vue'
+import data from '@/data.json'
+import { ref, onMounted } from 'vue'
+console.log(data)
+
+const bookmarkedList = ref()
+
+onMounted(() => bookmarked())
+
+const bookmarked = () => {
+  bookmarkedList.value = data.filter((data) => data.isBookmarked)
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/main.scss';
+.carrousel {
+  display: flex;
+  gap: pxToRem(40);
+
+  &-card {
+    width: pxToRem(470);
+    height: pxToRem(230);
+  }
+}
+
+.heading-secondary {
+  @extend %heading-large;
+}
+
+.grid-layout {
+  display: grid;
+  grid-template-columns: repeat(4, 280px);
+  gap: pxToRem(40);
+}
+</style>
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>movies</h1>
+  <h2 class="heading-secondary">Movies</h2>
+  <div class="grid-layout">
+    <MovieCard
+      v-for="movie in movieList"
+      :key="movie.title"
+      :category="movie.category"
+      :isBookmarked="movie.isBookmarked"
+      :isTrending="movie.isTrending"
+      :thumbnail="movie.thumbnail"
+      :title="movie.title"
+      :rating="movie.rating"
+      :year="movie.year"
+      class="movie-card"
+    />
+  </div>
 </template>
+
+<script setup>
+import MovieCard from '@/components/MovieCard.vue'
+import data from '@/data.json'
+import { ref, onMounted } from 'vue'
+console.log(data)
+
+const movieList = ref()
+
+onMounted(() => movies())
+
+const movies = () => {
+  movieList.value = data.filter((data) => data.category === 'Movie')
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/main.scss';
+.carrousel {
+  display: flex;
+  gap: pxToRem(40);
+
+  &-card {
+    width: pxToRem(470);
+    height: pxToRem(230);
+  }
+}
+
+.heading-secondary {
+  @extend %heading-large;
+}
+
+.grid-layout {
+  display: grid;
+  grid-template-columns: repeat(4, 280px);
+  gap: pxToRem(40);
+}
+</style>
