2d4922e7bea3819022b5affac454e5228fc6e932
cristinakellyt <cristinakellyt@gmail.com>
2024-01-16 16:50:50 +0100

    style(Auth): create login and signup styles

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,32 +1,213 @@
 <template>
-  <h1>Implement SignUp</h1>
-  <button class="back-btn" @click="goBack">Back</button>
+  <div class="auth-page">
+    <img src="/logo.svg" />
+    <div class="login-card">
+      <h1>Signup</h1>
+      <form class="form" id="login-form" @submit.prevent="signup">
+        <div class="form-group">
+          <input
+            class="form-group__input"
+            v-model="email"
+            type="email"
+            id="email"
+            placeholder="Email address"
+            autocomplete="email"
+            required
+          />
+          <label class="form-group__label" for="email">Email address</label>
+        </div>
+
+        <div class="form-group">
+          <input
+            class="form-group__input"
+            v-model="password"
+            type="password"
+            id="password"
+            placeholder="Password"
+            autocomplete="off"
+            required
+          />
+          <label class="form-group__label" for="password">Password</label>
+        </div>
+
+        <div class="form-group">
+          <input
+            class="form-group__input"
+            v-model="confirmPassword"
+            type="password"
+            id="confirm-password"
+            placeholder="Confirm Password"
+            autocomplete="off"
+            required
+          />
+          <label class="form-group__label" for="password"> Confirm Password</label>
+        </div>
+
+        <div class="form-group__btns-options">
+          <button type="submit" form="login-form">Create an account</button>
+          <span class=""
+            >Already have an account? <RouterLink :to="{ name: 'login' }"> Login</RouterLink>
+          </span>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-const router = useRouter()
+import { ref } from 'vue'
 
-const goBack = () => {
-  router.back()
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+
+const signup = () => {
+  console.log('signup')
+  //TODO: redirect to login page and clean form
 }
 </script>
 
 <style scoped lang="scss">
 @import '@/assets/scss/main.scss';
 
-.back-btn {
-  align-self: flex-start;
-  padding: pxToRem(10);
-  border: 1px solid $red;
-  background-color: transparent;
-  color: $red;
-  font-weight: 600;
-  transition: all 0.3s;
+.auth-page {
+  @extend %flex-column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  padding: pxToRem(32);
 
-  &:hover {
+  img {
+    margin-bottom: pxToRem(32);
+  }
+}
+
+.login-card {
+  @extend %flex-column;
+  width: pxToRem(400);
+  height: max-content;
+  background-color: $semi-dark-blue;
+  border-radius: pxToRem(12);
+  padding: pxToRem(32);
+  box-shadow: 0 pxToRem(4) pxToRem(16) rgba(0, 0, 0, 0.25);
+  margin-top: pxToRem(32);
+
+  h1 {
+    @extend %heading-large;
+    margin-bottom: pxToRem(32);
+  }
+
+  .form {
+    @extend %flex-column;
+    width: 100%;
+  }
+}
+
+.form-group {
+  @extend %flex-column;
+  width: 100%;
+  position: relative;
+
+  &__label {
+    opacity: 0;
+    position: absolute;
+    top: 40%;
+    left: pxToRem(16);
+    transform: translateY(-50%);
+    font-size: pxToRem(14);
+    font-weight: 300;
+    line-height: pxToRem(17);
+    font-family: inherit;
+    color: $greyish-blue;
+    display: block;
+    z-index: 100;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &__input {
+    height: pxToRem(48);
+    width: 100%;
+    border: none;
+    padding: pxToRem(12) pxToRem(12) pxToRem(12) 0;
+    background-color: $semi-dark-blue;
+    font-size: pxToRem(14);
+    border-bottom: pxToRem(2) solid $greyish-blue;
+    font-weight: 400;
+    line-height: pxToRem(19);
+    color: $pure-white;
+    margin-bottom: pxToRem(24);
+    transition: all 0.2s ease-in-out;
+
+    &::placeholder {
+      color: $greyish-blue;
+      font-size: pxToRem(14);
+      font-weight: 400;
+      font-family: inherit;
+    }
+
+    &:focus {
+      outline: none;
+      border-bottom: pxToRem(2) solid $pure-white;
+    }
+
+    &:not(:placeholder-shown) + .form-group__label {
+      opacity: 1;
+      top: 0;
+      left: 0;
+      font-size: pxToRem(14);
+    }
+  }
+
+  .eye-password {
+    display: inline-block;
+    height: pxToRem(24);
+    width: pxToRem(24);
+    position: absolute;
+    top: 40%;
+    right: 0;
+    transform: translateY(-50%);
     cursor: pointer;
-    transform: translateY(-4px);
+  }
+}
+
+.form-group__btns-options {
+  @extend %flex-column;
+  gap: pxToRem(24);
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+  margin-top: pxToRem(16);
+
+  span {
+    @extend %body-medium;
+  }
+
+  a {
+    color: $red;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+  }
+
+  a:hover {
+    color: $pure-white;
+  }
+
+  button {
+    height: pxToRem(48);
+    width: 100%;
+    border: none;
+    border-radius: pxToRem(6);
+    background-color: $red;
+    font-size: pxToRem(14);
+    font-weight: 500;
+    color: $pure-white;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: $pure-white;
+      color: $semi-dark-blue;
+    }
   }
 }
 </style>
